a88d2e4f4ef7fa0a69b2b9798cfa043ea22e557f
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-10-26 18:11:40 -0700

    Update jsquiz.js

--- jsquiz.js
+++ jsquiz.js
@@ -35,7 +35,7 @@ function billQuestions() {
       correctAnswer: 'd'
     }
   ];
-console.log(myBills[1].choices);
+console.log(myBills.choices);
 console.log(myBills);
 
 let questionCounter = 0; /* Tracks question number */
@@ -44,17 +44,87 @@ let selections = []; /* Array containing user choices */
 console.log (selections);
 let quiz = $('#quiz'); /* Quiz div object */
 
+/* Pulls the length of the Choices Objects */
+getChoicesLength();
 
-getChoicesKeys();
-
-function getChoicesKeys () {
-    let length = Object.keys(myBills[1].choices).length;
-    console.log(length);
-    return(length);
+function getChoicesLength () {
+    let clength = Object.keys(myBills[1].choices).length;
+    console.log(clength);
+    return(clength);
 }
-        
+
+getChoicesKey();
+
+function getChoicesKey () {
+    let cKey = Object.keys(myBills[1].choices);
+    console.log(cKey);
+    return(cKey);
+}
+
+getChoicesValue();
+
+function getChoicesValue () {
+    let cValue = Object.values(myBills[1].choices);
+    console.log(cValue);
+    return(cValue);
+}
+
+
 /* Display initial question */
 displayNext();
+
+/* Creates and returns the div that contains the questions and 
+*  the answer selections */
+function createQuestionElement(index) {
+  let qElement = $('<div>', {
+      id: 'question'
+  });
+  
+  let header = $('<h2>Question ' + (index + 1) + ':</h2>');
+      qElement.append(header);
+  
+  let question = $('<p>').append(myBills[index].question);
+      qElement.append(question);
+  
+  let radioButtons = $(createRadios(index));
+      console.log(radioButtons);    
+  qElement.append(radioButtons);
+  
+  return qElement;
+}
+
+/* Creates a list of the answer choices as radio inputs */
+function createRadios(index) {
+  let radioList = $('<ul>');
+  // let item = $('<li>');
+  let input = $('');
+clength = getChoicesLength();
+cKey = getChoicesKey();
+cValue = getChoicesValue();
+
+
+  for (let i = 0; i < clength; i++) {
+      // if (getChoicesKey(myBills))
+      input = $('<li><input type="radio" name="answer" />' + myBills[i].choices + '</input></li>');
+      radioList.append(input);
+      // input += myBills[index].choices[i];
+
+      console.log(radioList);
+      
+      
+      console.log(input);
+      console.log(myBills.choices);
+      // item.append(input);
+  }
+
+  return radioList;
+}
+
+/* Reads the user selection and pushes the value to an array */
+function choose() {
+  selections[questionCounter] = +$('input[name="answer"]:checked').val();
+}
+
 
 /* Click handler for the 'next' button */
 $('#next').on('click', function (e) {e.preventDefault();
@@ -99,55 +169,13 @@ $('.button').on('mouseleave', function () {
   $(this).removeClass('active');
 });
 
-/* Creates and returns the div that contains the questions and 
-*  the answer selections */
-function createQuestionElement(index) {
-  let qElement = $('<div>', {
-      id: 'question'
-  });
-  
-  let header = $('<h2>Question ' + (index + 1) + ':</h2>');
-      qElement.append(header);
-  
-  let question = $('<p>').append(myBills[index].question);
-      qElement.append(question);
-  
-  let radioButtons = $(createRadios(index));
-      qElement.append(radioButtons);
-  
-  return qElement;
-}
-
-/* Creates a list of the answer choices as radio inputs */
-function createRadios(index) {
-  let radioList = $('<ul>');
-  let item = $('<li>');
-  let input = $('');
-
-  for (let i = 0; i < length; i++) {
-      input = $('<input type="radio" name="answer" value=' + myBills[i].choices + ' />');
-      input += myBills[1].choices;
-      item.append(input);
-      radioList.append(item);
-  }
-  console.log(radioList);
-  console.log(item);
-  console.log(input);
-
-  return radioList;
-}
-
-/* Reads the user selection and pushes the value to an array */
-function choose() {
-  selections[questionCounter] = +$('input[name="answer"]:checked').val();
-}
-
 /* Displays next requested element */
 function displayNext() {
   quiz.fadeOut(function() {
     $('#question').remove();
     
-    if(questionCounter < myBills.length){
+    clength = getChoicesLength();    
+    if(questionCounter < clength){
       let nextQuestion = createQuestionElement(questionCounter);
       quiz.append(nextQuestion).fadeIn();
       if (!(isNaN(selections[questionCounter]))) {
@@ -184,7 +212,7 @@ function displayScore() {
   }
   
   score.append('You got ' + numCorrect + ' questions out of ' +
-               myBills.length + ' right!!!');
+               clength + ' right!!!');
   return score;
 } 
 }
