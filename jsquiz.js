b403316bf7cf03091bc222a869556ac54a319030
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-11-03 14:13:07 -0700

    Update jsquiz.js

--- jsquiz.js
+++ jsquiz.js
@@ -4,74 +4,96 @@ class quiz {
         this.choices = choices;
         this.correctAnswer = correctAnswer;
     } 
-
   }
   
 function billQuestions() {
   const myBills = [
     { 
       question: 'Who is on the One Dollar Bill?',
-      choices: {a: 'Thomas Jefferson', b: 'George Washington', c: 'James Knox Polk', d: 'Benjamin Franklin'},
-      correctAnswer: 'b'
+      choices: ['Thomas Jefferson', 'George Washington', 'James Knox Polk', 'Benjamin Franklin'],
+      correctAnswer: 'George Washington'
     },
     {
       question: 'Who is on the Fifty Dollar Bill?',
-      choices: {a: 'George Washington', b: 'John Quincy Adams', c: 'Ulysses S. Grant', d: 'Winston Churchill'},
-      correctAnswer: 'c'
+      choices: ['George Washington', 'John Quincy Adams', 'Ulysses S. Grant', 'Winston Churchill'],
+      correctAnswer: 'John Quincy Adams'
     },
     {
       question: 'Who is on the Five Dollar Bill?',
-      choices: {a: 'Abraham Lincoln', b: 'James Monroe', c: 'Andrew Jackson', d: 'Ulysses S. Grant'},
-      correctAnswer: 'a'
+      choices: ['Abraham Lincoln', 'James Monroe', 'Andrew Jackson', 'Ulysses S. Grant'],
+      correctAnswer: 'Abraham Lincoln'
     },
     {
       question: 'Who is on the Two Dollar Bill?',
-      choices: {a: 'Andrew Johnson', b: 'John Marshall',c: 'John Adams', d: 'Thomas Jefferson'},
-      correctAnswer: 'd'
+      choices: ['Andrew Johnson', 'John Marshall','John Adams', 'Thomas Jefferson'],
+      correctAnswer: 'Thomas Jefferson'
     },
     {
       question: 'Who is on the Hundred Dollar Bill?',
-      choices: {a: 'James Madison', b: 'Woodrow Wilson', c: 'James Buchanan', d: 'Benjamin Franklin'},
-      correctAnswer: 'd'
+      choices: ['James Madison', 'Woodrow Wilson', 'James Buchanan', 'Benjamin Franklin'],
+      correctAnswer: 'Benjamin Franklin'
     }
   ];
-console.log(myBills.choices);
-console.log(myBills);
 
-let questionCounter = 0; /* Tracks question number */
+/* Tracks question number */
+let questionCounter = 0; 
 console.log (questionCounter);
 let selections = []; /* Array containing user choices */
 console.log (selections);
 let quiz = $('#quiz'); /* Quiz div object */
 
+
 /* Pulls the length of the Choices Objects */
 getChoicesLength();
-
 function getChoicesLength () {
     let clength = Object.keys(myBills[1].choices).length;
-    console.log(clength);
+    // console.log(clength);
     return(clength);
 }
 
-getChoicesKey();
-
-function getChoicesKey () {
-    let cKey = Object.keys(myBills[1].choices);
-    console.log(cKey);
-    return(cKey);
-}
-
-getChoicesValue();
-
-function getChoicesValue () {
-    let cValue = Object.values(myBills[1].choices);
-    console.log(cValue);
-    return(cValue);
-}
-
-
 /* Display initial question */
 displayNext();
+
+/* Click handler for the 'next' button */
+$('#next').on('click', function (e) {e.preventDefault();
+  
+/* Suspend click listener during fade animation */
+    if(quiz.is(':animated')) {return false;} choose();
+  
+/* If no user selection, progress is stopped */
+    if(!(isNaN(selections[questionCounter]))) 
+    {alert('Please make a selection!');
+} else {questionCounter++;
+    displayNext();
+  }
+});
+
+/* Click handler for the 'prev' button */
+$('#prev').on('click', function (e) { e.preventDefault();
+  if(quiz.is(':animated')) {return false;}
+    choose();
+    questionCounter--;
+    displayNext();
+});
+
+/* Click handler for the 'Start Over' button */
+$('#start').on('click', function (e) { e.preventDefault();
+    if(quiz.is(':animated')) {
+    return false;
+  }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+});
+
+/* Animates buttons on hover */
+$('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+});
+$('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+});
 
 /* Creates and returns the div that contains the questions and 
 *  the answer selections */
@@ -87,99 +109,47 @@ function createQuestionElement(index) {
       qElement.append(question);
   
   let radioButtons = $(createRadios(index));
-      console.log(radioButtons);    
-  qElement.append(radioButtons);
-  
+      qElement.append(radioButtons);
   return qElement;
 }
 
 /* Creates a list of the answer choices as radio inputs */
 function createRadios(index) {
   let radioList = $('<ul>');
-  // let item = $('<li>');
   let input = $('');
 clength = getChoicesLength();
-cKey = getChoicesKey();
-cValue = getChoicesValue();
-
 
   for (let i = 0; i < clength; i++) {
-      // if (getChoicesKey(myBills))
-      input = $('<li><input type="radio" name="answer" />' + myBills[i].choices + '</input></li>');
+    // for (let j = 0; j < myBills[0].question[i].length; j++) {
+      input = $('<li><input type="radio" name="answer" />' + myBills[index].choices[i] + '</input></li>');
       radioList.append(input);
-      // input += myBills[index].choices[i];
-
-      console.log(radioList);
-      
-      
       console.log(input);
-      console.log(myBills.choices);
-      // item.append(input);
-  }
-
+      console.log('Im here');
+      console.log(myBills[index].choices[i]);
+  // }
+}
   return radioList;
 }
 
 /* Reads the user selection and pushes the value to an array */
 function choose() {
-  selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  selections[questionCounter] = $('input[name="answer"]:checked').serialize();
 }
-
-
-/* Click handler for the 'next' button */
-$('#next').on('click', function (e) {e.preventDefault();
-  
-/* Suspend click listener during fade animation */
-  if(quiz.is(':animated')) {return false;} choose();
-  
-/* If no user selection, progress is stopped */
-  if (isNaN(selections[questionCounter])) 
-    {alert('Please make a selection!');
-} else {questionCounter++;
-    displayNext();
-  }
-});
-
-/* Click handler for the 'prev' button */
-$('#prev').on('click', function (e) { e.preventDefault();
- if(quiz.is(':animated')) {return false;}
-  choose();
-  questionCounter--;
-  displayNext();
-});
-
-/* Click handler for the 'Start Over' button */
-$('#start').on('click', function (e) {
-  e.preventDefault();
-  
-  if(quiz.is(':animated')) {
-    return false;
-  }
-  questionCounter = 0;
-  selections = [];
-  displayNext();
-  $('#start').hide();
-});
-
-/* Animates buttons on hover */
-$('.button').on('mouseenter', function () {
-  $(this).addClass('active');
-});
-$('.button').on('mouseleave', function () {
-  $(this).removeClass('active');
-});
 
 /* Displays next requested element */
 function displayNext() {
   quiz.fadeOut(function() {
     $('#question').remove();
     
-    clength = getChoicesLength();    
+    clength = getChoicesLength();  
+      
     if(questionCounter < clength){
       let nextQuestion = createQuestionElement(questionCounter);
       quiz.append(nextQuestion).fadeIn();
-      if (!(isNaN(selections[questionCounter]))) {
+      if ((isNaN(selections[questionCounter]))) {
         $('input[value='+selections[questionCounter]+']').prop('checked', true);
+        console.log(selections);
+        console.log(questionCounter);
       }
       
 /* Controls display of 'prev' button */
@@ -206,13 +176,13 @@ function displayScore() {
   
   let numCorrect = 0;
   for (var i = 0; i < selections.length; i++) {
-    if (selections[i] === myBills[i].correctAnswer) {
+
+    if (selections[i] === myBills[i].correctAnswer.value) {
       numCorrect++;
     }
   }
   
-  score.append('You got ' + numCorrect + ' questions out of ' +
-               clength + ' right!!!');
+  score.append('You got ' + numCorrect + ' questions out of ' + clength + ' right!!!');
   return score;
 } 
 }
