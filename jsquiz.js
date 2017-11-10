e45a03cbc61b733dc1a369b322d5cef4d2f4e474
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2017-11-10 12:29:16 -0700

    Update jsquiz.js

--- jsquiz.js
+++ jsquiz.js
@@ -10,33 +10,32 @@ function startAlert() {
   alert('Test Your Skills!')
 }
 
-
 function billQuestions() {
   const myBills = [
     { 
       question: 'Who is on the One Dollar Bill?',
       choices: ['Make a Selection', 'Martin Van Buren', 'George Washington', 'James Knox Polk', 'Benjamin Franklin'],
-      correctAnswer: ['George Washington']
+      correctAnswer: 'George Washington'
     },
     {
       question: 'Who is on the Fifty Dollar Bill?',
       choices: ['Make a Selection', 'John Tyler', 'John Quincy Adams', 'Ulysses S. Grant', 'Winston Churchill'],
-      correctAnswer: ['John Quincy Adams']
+      correctAnswer: 'John Quincy Adams'
     },
     {
       question: 'Who is on the Five Dollar Bill?',
       choices: ['Make a Selection', 'Abraham Lincoln', 'James Monroe', 'Andrew Jackson', 'Ulysses S. Grant'],
-      correctAnswer: ['Abraham Lincoln']
+      correctAnswer: 'Abraham Lincoln'
     },
     {
       question: 'Who is on the Two Dollar Bill?',
       choices: ['Make a Selection', 'Andrew Johnson', 'John Marshall','John Adams', 'Thomas Jefferson'],
-      correctAnswer: ['Thomas Jefferson']
+      correctAnswer: 'Thomas Jefferson'
     },
     {
       question: 'Who is on the Hundred Dollar Bill?',
       choices: ['Make a Selection', 'James Madison', 'Woodrow Wilson', 'James Buchanan', 'Benjamin Franklin'],
-      correctAnswer: ['Benjamin Franklin']
+      correctAnswer: 'Benjamin Franklin'
     }
   ];
 
@@ -44,31 +43,15 @@ function billQuestions() {
 let questionCounter = 0; 
 console.log (questionCounter);
 let selected = new Array (); /* Array containing user choices */
-// selected.toString();
-console.log (selected);
+
 let quiz = $('#quiz'); /* Quiz div object */
-
-
-/* Pulls correctAnswer Values */
-
-getAnswerValues(); 
-function getAnswerValues() {
-  for (let i = 0; i < myBills[i].correctAnswer.length; i++) {
-   let aValues = Object.values(myBills[i].correctAnswer);
-   console.log(aValues);
-  }
-};
-
 
 /* Pulls the length of the Choices Objects */
 getChoicesLength();
 function getChoicesLength () {
     let clength = Object.keys(myBills[1].choices).length;
-    // console.log(clength);
-    return(clength);
+  return(clength);
 }
-
-
 
 /* Display initial question */
 displayNext();
@@ -83,26 +66,23 @@ $('#next').on('click', function (e) {e.preventDefault();
     getSelectedValues();
     function getSelectedValues() {
         let sValues = Object.values(selected);
-        console.log(sValues);
-  for (i = 0; i < selected.length; i++) {
-    if(sValues[i] === "Make a Selection") {
+
+    if(selected[selected.length -1] === "Make a Selection") {
         alert('Make a Selection to Move Forward');
-        console.log('Before Question Counter');
-        console.log(sValues);
-            questionCounter--;
+        console.log(selected);
+            // questionCounter--;
               selected.pop();
-              console.log(sValues);
+              
+            console.log(selected);
     } else {
       console.log(questionCounter);
         questionCounter++;
-           console.log (questionCounter);
-           console.log('Passed Question Counter' + i);
+        console.log(selected);
         displayNext();
-      }
+      
     }
   }
 });
-
 
 /* Click handler for the 'prev' button */
 $('#prev').on('click', function (e) { e.preventDefault();
@@ -155,13 +135,10 @@ function List(index) {
   let dropDown = $('<select>');
   let input = $('');
 clength = getChoicesLength();
-
   for (let i = 0; i < clength; i++) {
       input = $('<option> ' + myBills[index].choices[i] + '</option>');
       dropDown.append(input);
-      console.log('new dropdown' + i);
 }
-
   return dropDown;
 }
  
@@ -170,20 +147,15 @@ function choose() {
       $('option:selected').each(function() {
         selected.push($(this).val());
       });
-      // console.log(selected);
-      console.log(questionCounter);      
 };
 
 /* Displays next requested element */
 function displayNext() {
   quiz.fadeOut(function() {
       $('#question').remove();
-      console.log(myBills.length);
-      console.log(questionCounter);
-      console.log('Next Question');
         if(questionCounter < myBills.length){
-          console.log(myBills.length);
-          console.log('Show Next Queston');
+          // console.log(myBills.length);
+          // console.log('Show Next Queston');
             let nextQuestion = createQuestionElement(questionCounter);
         quiz.append(nextQuestion).fadeIn();
     
@@ -191,7 +163,6 @@ function displayNext() {
   if(questionCounter === 1){
       $('#prev').show();
   } else if(questionCounter === 0){
-
       $('#prev').hide();
       $('#next').show();  
       }
@@ -206,44 +177,29 @@ function displayNext() {
 }
 
 /* Computes score and returns a paragraph element to be displayed */
-
-
-
-displayScore();
 function displayScore() {
-  let score = $('<p>',{id: 'question'});
+  let score = $('<p>', {id: 'question'});
 
-  getSelectedValues();
-  function getSelectedValues() {
-      let sValues = Object.values(selected);
-      console.log(sValues);
-
-  getAnswerValues(); 
-  function getAnswerValues() {
-      for (let j = 0; j < myBills[j].correctAnswer.length; j++) {
-      let aValues = Object.values(myBills[j].correctAnswer);
-      console.log(aValues);
-
-  let scoreValue = [];   
   let numCorrect = 0;
-    for (var i = 0; i < selected.length; i++) {
-        if (sValues[i] === aValues[i]) {
-          numCorrect++;
-          scoreValue.push(25);
-          console.log(scoreValue);
-          console.log(numCorrect);
-          console.log('im here');
-    }
-  score.append('You got ' + numCorrect + ' questions out of ' + questionCounter + ' right. Your Score is ' + scoreValue/100 );
-  return score;
-  console.log(score);
-        }
+    for (var i = 0; i < myBills.length; i++) {
+      if (selected[i] === myBills[i].correctAnswer) {
+        numCorrect++;
+        console.log(numCorrect);
+        console.log(selected[i]);
+        console.log(myBills[i].correctAnswer);
+        grade = numCorrect * 20
+        console.log(grade);
       }
     }
-  }
-}
+   
+    
+  let final = ('You got ' + numCorrect + ' questions out of ' + questionCounter + ' right. Your Score is ' + grade + '!');
+        score.append(final);
+    return score;
 
+}
 };
+
 
 startAlert();
 billQuestions();
